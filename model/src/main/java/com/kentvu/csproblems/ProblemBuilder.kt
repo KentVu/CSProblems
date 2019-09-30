4c54286a2f9cd287579bb050d455134d630b511d
Vu Tran Kien <vutrankien.vn@gmail.com>
2019-09-30 10:42:39 +0900

    WIP: Phases definition (to help parsing)

--- model/src/main/java/com/kentvu/csproblems/ProblemBuilder.kt
+++ model/src/main/java/com/kentvu/csproblems/ProblemBuilder.kt
@@ -2,7 +2,7 @@ package com.kentvu.csproblems
 
 class ProblemBuilder {
     private val description: String? = null
-    private val title: String? = null
+    var title: String? = null
     fun build(): Problem {
         if (title == null) {
             throw IllegalStateException("title is null")
--- model/src/main/java/com/kentvu/csproblems/Repo.kt
+++ model/src/main/java/com/kentvu/csproblems/Repo.kt
@@ -1,20 +1,26 @@
 package com.kentvu.csproblems
 
 class Repo(data: String) {
+    enum class Phase(val sig:String, addImpl: (ProblemBuilder, String) -> Unit) {
+        Title(".title", {prb, text -> prb.title = text});
+        companion object {
+            private val values = values()
+
+            fun sigMatch(sig:String) = values.any {it.sig.equals(sig)}
+        }
+    }
     val problems = mutableListOf<Problem>()
 
     fun problems() = problems.toList()
 
     init {
-        data.lineSequence().forEach {
+        data.lineSequence().forEach { line ->
             var currentProblem = ProblemBuilder()
-            when(it) {
-                ".title" -> {
-                    currentProblem = ProblemBuilder()
-                    problems.add(currentProblem.build())
-                }
-                else -> ""
+            if (Phase.sigMatch(line)) {
+                currentProblem = ProblemBuilder()
+                problems.add(currentProblem.build())
             }
+            else ""
             TODO("Use RegEx™️")
         }
     }
